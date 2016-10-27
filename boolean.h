d73fc727a12439188df14a728854889d10f650e4
18265716 <18265716@localhost.curtin.edu.au>
2016-10-27 13:55:58 +0800

    Added comments to insertionsort.c without breakin shit

--- boolean.h
+++ boolean.h
@@ -1,3 +1,10 @@
-/* Defines boolean variables. Used for readability as C is a mess already */
+/**
+* FILE:			boolean.h
+* AUTHOR:		Brandyn Britton
+* USERNAME:		18265716
+* UNIT:			UCP
+* PURPOSE:		UCP Assignment 2016 Semester 2
+* COMMENTS:		Defines values for booleans 
+*/
 #define TRUE 1
 #define FALSE 0
--- insertionsort.c
+++ insertionsort.c
@@ -7,7 +7,7 @@
 #include "boolean.h"
 
 static int sortByInt(int*, int*, int);
-int sortByString(char*, char*, int);
+static int sortByString(char*, char*, int);
 
 void sortList(LinkedList* dataList, HeaderInfo* header, int order)
 {
@@ -18,38 +18,53 @@ void sortList(LinkedList* dataList, HeaderInfo* header, int order)
 
     int index = header->index;
 
+    /* Loop through outer list and get all inner nodes */
     for (i = 1; i < dataList->size - 1; i++)
     {
         posFound = FALSE;
         j =  i;
+
+        /* Compare current node with previous node if it exists */
         while ((j > 0) && (!posFound))
         {
+            /* Get list values from the nodes */
             curList = (LinkedList*) findIndex(dataList, j);
             prevList = (LinkedList*) findIndex(dataList, j - 1);
 
+            /* Get void* values from the lists */
             curValue = findIndex(curList, index);
             prevValue = findIndex(prevList, index);
 
             if (strcmp(header->type, "integer") == 0)
             {
+                /* Values are ints so compare */
                 posFound = sortByInt((int*) curValue, (int*) prevValue, order);
             }
             else
             {
+                /* Values are strings so compare */
                 posFound = sortByString((char*) curValue, (char*) prevValue, order);
             }
 
             if (!posFound)
             {
+                /* Swap nodes as incorrect position */
                 swapNodesByIndex(dataList, j, j - 1);
             }
 
-            j --;
+            j -= 1;
 
         }
     }
 }
 
+/**
+ * SUBMODULE: sortByInt
+ * IMPORT: current, prev, order
+ * EXPORT: posFound
+ * Compares two ints and ensures that they are in the 
+ * currect position
+ */
 int sortByInt(int* current, int* prev, int order)
 {
     int posFound = TRUE;
@@ -85,6 +100,13 @@ int sortByInt(int* current, int* prev, int order)
     return posFound;
 }
 
+/**
+ * SUBMODULE: sortByString
+ * IMPORT: current, prev, order
+ * EXPORT: posFound
+ * Compares two strings and ensures that they are in the 
+ * currect position
+ */
 int sortByString(char* current, char* prev, int order)
 {
     int posFound = TRUE;
